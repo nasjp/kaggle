ebf8273241eb9acb347f4da78168a1f61e04ace4
nas <y.softvalley@hey.com>
2022-02-05 18:34:19 +0900

    titanic

--- competition.bash
+++ competition.bash
@@ -2,26 +2,13 @@
 
 competition=$1
 
-function activate_venv() {
-		source .venv/bin/activate
-}
+if [ -z "$competition" ]; then
+	echo "Please provide a competition name"
+	exit 1
+fi
 
-function validate_competition() {
-		if [ -z "$competition" ]; then
-				echo "Please provide a competition name"
-				exit 1
-		fi
-}
-
-function download_competition_data() {
-		kaggle competitions download -c $competition
-}
-
-function extract_competition_data() {
-		kaggle competitions datasets -c $competition
-}
-
-
-activate_venv
-validate_competition
-download_competition_data
+source .venv/bin/activate
+kaggle competitions download -c $competition --force
+unzip -f $competition.zip -d input/$competition
+cp template.ipynb working/$competition.ipynb
+rm $competition.zip

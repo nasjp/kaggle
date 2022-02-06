e3849bbd00e3a919e7da112cc534d67d4e052296
nas <y.softvalley@hey.com>
2022-02-06 20:10:09 +0900

    init home-credit-default-risk

--- competition.bash
+++ competition.bash
@@ -9,7 +9,7 @@ fi
 
 source .venv/bin/activate
 kaggle competitions download -c $competition --force
-unzip -f $competition.zip -d input/$competition
+unzip -o $competition.zip -d input/$competition
 cp template.ipynb working/$competition.ipynb
 mkdir -p output/$competition
 rm $competition.zip
